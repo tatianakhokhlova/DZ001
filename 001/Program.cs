8a1fc3c73423ba9088d26480fd61e6cb646c66ab
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-09 15:11:46 +0300

    Решение задачи

---
+++ 001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+
+if(a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("Данное число кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Данное число не кратно 7 и 23");
+}
