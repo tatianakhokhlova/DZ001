bae9510aa3629e07e5c54065eedc8149ce45fe78
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-09 16:43:18 +0300

    Задать номер четверти, показать диапазоны для возможных координат

---
+++ dz003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine(number + " четверть содержит координаты: x>0 y>0");
+}
+else if (number == 2)
+{
+    Console.WriteLine(number + " четверть содержит координаты: x<0 y>0");
+}
+else if (number == 3)
+{
+    Console.WriteLine(number + " четверть содержит координаты: x<0 y<0");
+}
+else
+{
+    Console.WriteLine(number + " четверть содержит координаты: x>0 y<0");
+}
