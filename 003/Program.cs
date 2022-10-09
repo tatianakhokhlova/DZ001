23055ba6bcec0ad4e3811203b7005e571de2f529
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-09 15:51:20 +0300

    Поиск числа палиндрома

---
+++ 003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine());
+String b = a.ToString();
+
+if(b[0]==b[4] && b[1]==b[3])
+{
+    Console.WriteLine("число палиндром");
+}
+else
+{
+    Console.WriteLine("число не палиндром");
+}
