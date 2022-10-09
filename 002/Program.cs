4d4b1bfba93c9be235fb52b10d334b7892577379
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-09 15:44:54 +0300

    Решение задачи 2

---
+++ 002/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("введите значение x");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("введите значение y");
+int y = int.Parse(Console.ReadLine());
+
+if(x>0 && y>0)
+{
+    Console.WriteLine("первая четверть");
+}
+
+else if(x<0 && y>0)
+{
+    Console.WriteLine("вторая четверь");
+}
+
+else if(x<0 && y<0)
+{
+    Console.WriteLine("третья четверть");
+}
+else
+{
+    Console.WriteLine("четвертая четверть");
+}
