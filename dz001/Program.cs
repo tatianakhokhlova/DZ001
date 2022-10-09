060f765c130769ca279e15e2904990681f2c2cff
Tatyana <tatyana.dovganskaya@gmail.com>
2022-10-09 16:15:31 +0300

    Решение задачи про выходной день

---
+++ dz001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число, обозначающее день недели");
+int a = int.Parse(Console.ReadLine());
+
+if(a>6 || a==6)
+{
+    Console.WriteLine("Выходной день");
+}
+
+else
+
+{
+    Console.WriteLine("Не является выходным днем");
+}
